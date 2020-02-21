84b536386809bfaf79abaea59588efcace65f7a2
70kg111 <76104718@qq.com>
2020-02-21 14:33:24 +0800

    把 model 融入到 store

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,9 +1,27 @@
-import recordListModel from '@/models/recordListModel';
+import clone from '@/lib/clone';
 
-export default {
+const localStorageKeyName = 'recordList';
+
+const recordStore = {
   //record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => {
-    recordListModel.create(record);
-  }
-}
+  recordList: [] as RecordItem[],
+  fetchRecords() {
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
+  },
+  saveRecords() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
+  },
+  createRecord(record: RecordItem) {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    if (this.recordList) {
+      this.recordList.push(record2);
+    }
+    recordStore.saveRecords();
+  },
+};
+
+recordStore.fetchRecords();
+
+export default recordStore;
