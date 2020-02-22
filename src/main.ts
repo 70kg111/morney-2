3d3f2bb5e3bf0ed6c010211fd4121edae61de16c
70kg111 <76104718@qq.com>
2020-02-22 14:54:42 +0800

    提示用手机扫描二维码打开页面

--- src/main.ts
+++ src/main.ts
@@ -25,5 +25,17 @@ new Vue({
 window.onload = function () {
   setTimeout(function () {
     window.scrollTo(0, 10000);
-  },0);
+  }, 0);
 };
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('为了保证用户体验，建议使用手机打开本页面~');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img);
+}
